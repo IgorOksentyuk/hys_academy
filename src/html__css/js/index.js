803e0247f8f0efccd2647dbcd9d3f977495c5d23
Igor Oksentyuk <winbka.lem@gmail.com>
2022-10-28 12:54:25 +0300

    ls_8 | Imported class App and call init method

--- src/html__css/js/index.js
+++ src/html__css/js/index.js
@@ -1,6 +1,20 @@
 import '../styles/style.scss';
+
+import '../images/sprite.svg';
+import '../images/courses-block/woman-design.png';
+import '../images/courses-block/woman-marketing.png';
+import '../images/courses-block/woman-web.png';
+import '../images/tour-block/rectangle.png';
+import '../images/customers-block/customers-photo.png';
+
+import 'jquery';
+import 'slick-carousel';
 import './mobile-menu.js';
 import './paginator.js';
 import './slick.min.js';
 import './slider.js';
-import '../images/sprite.svg';
+
+import App from './app.js';
+
+const app = new App();
+app.init();
