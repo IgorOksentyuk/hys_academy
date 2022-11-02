dceeb431cc2b3b826ddd0ea240636ec792173a66
Igor Oksentyuk <winbka.lem@gmail.com>
2022-11-02 16:10:10 +0200

    ls_9 | Made fetch request for getting data, made select logic

--- src/html__css/js/app.js
+++ src/html__css/js/app.js
@@ -1,26 +1,42 @@
 import Slider from './slider.js';
 import Storage from './storage.js';
+import Select from './select.js';
 
 export default class App {
   init() {
+    // Initialized all components.
     const slider = new Slider('#slider');
+    const select = new Select('#select');
     const storage = new Storage();
-    const allSlides = storage.getSliderData();
 
-    slider.setData(allSlides);
+    fetch(`https://jsonplaceholder.typicode.com/albums/1/photos`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        slider.setData(data.slice(0, 8));
 
-    slider.render();
+        slider.render();
+        onButtonsClick();
+      });
 
-    const btnLeft = document.querySelector('.circle.left');
-    const btnRight = document.querySelector('.circle.right');
+    select.render();
 
-    btnLeft.addEventListener('click', () => {
-      slider.handleLeftClick();
-    });
-    btnRight.addEventListener('click', () => {
-      slider.handleRightClick();
-    });
+    // Slider logic.
+    const onButtonsClick = () => {
+      const btnLeft = document.querySelector('.circle.left');
+      const btnRight = document.querySelector('.circle.right');
 
+      btnLeft.addEventListener('click', () => {
+        slider.handleLeftClick();
+      });
+
+      btnRight.addEventListener('click', () => {
+        slider.handleRightClick();
+      });
+    };
+
+    // Forms logic.
     const form = document.getElementById('form');
     const nameInput = document.getElementById('name-input');
     const phoneInput = document.getElementById('phone-input');
@@ -48,7 +64,54 @@ export default class App {
       storage.clearEmail();
     });
 
-    //Slick slider for Courses section//
+    // --> Select logic.
+    const dropdownBtn = document.querySelector('.prefer__select-btn');
+    const dropdownList = document.querySelector('.prefer__select-list');
+
+    // Click on button; Open/close dropdown list.
+    dropdownBtn.addEventListener('click', () => {
+      dropdownList.classList.toggle('prefer__select-list-visible');
+      dropdownBtn.classList.add('prefer__select-btn--active');
+    });
+
+    // Click on item, choose text of item, save focus os item.
+    dropdownList.onclick = (e) => {
+      e.stopPropagation();
+      const currentItem = e.target;
+      const currentItemNumber = currentItem.getAttribute('data-value');
+      dropdownBtn.innerText = currentItem.innerText;
+      dropdownList.classList.remove('prefer__select-list-visible');
+      dropdownBtn.focus();
+      const selectedAlbum = select.onSelectChange(currentItemNumber);
+
+      fetch(`https://jsonplaceholder.typicode.com/albums/${selectedAlbum}/photos`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          slider.setData(data.slice(0, 8));
+
+          slider.render();
+          onButtonsClick();
+        });
+    };
+
+    //Click outside button -> closed dropdown, remove focus from button.
+    document.addEventListener('click', (e) => {
+      if (e.target !== dropdownBtn) {
+        dropdownList.classList.remove('prefer__select-list-visible');
+        dropdownBtn.classList.remove('prefer__select-btn--active');
+      }
+    });
+
+    //Press on Esc or Tab closed dropdown.
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' || e.key === 'Tab') {
+        dropdownList.classList.remove('prefer__select-list-visible');
+      }
+    });
+
+    // --> Slick slider logic.
 
     $(document).ready(function () {
       $('.courses__box').slick({
