2705e1c74b28d0f72b6ffc1f7c1c2c3ceb7f947e
Igor Oksentyuk <winbka.lem@gmail.com>
2022-11-03 11:50:43 +0200

    ls_9 | Created init method and move all select logic in select file

--- src/html__css/js/select.js
+++ src/html__css/js/select.js
@@ -1,7 +1,49 @@
 export default class Select {
   #el;
-  constructor(selector) {
+  #dropdownList;
+  #dropdownBtn;
+  constructor(selector, onSelectChange) {
     this.#el = document.querySelector(selector);
+    this.render();
+    this.init(onSelectChange);
+  }
+
+  init(onSelectChange) {
+    // --> Select logic.
+    this.#dropdownBtn = document.querySelector('.prefer__select-btn');
+    this.#dropdownList = document.querySelector('.prefer__select-list');
+
+    // Click on button; Open/close dropdown list.
+    this.#dropdownBtn.addEventListener('click', () => {
+      this.#dropdownList.classList.toggle('prefer__select-list-visible');
+      this.#dropdownBtn.classList.add('prefer__select-btn--active');
+    });
+
+    // Click on item, choose text of item, save focus os item.
+    this.#dropdownList.onclick = (e) => {
+      e.stopPropagation();
+      const currentItem = e.target;
+      const currentItemNumber = currentItem.getAttribute('data-value');
+      this.#dropdownBtn.innerText = currentItem.innerText;
+      this.#dropdownList.classList.remove('prefer__select-list-visible');
+      this.#dropdownBtn.focus();
+      onSelectChange(currentItemNumber);
+    };
+
+    //Click outside button -> closed dropdown, remove focus from button.
+    document.addEventListener('click', (e) => {
+      if (e.target !== this.#dropdownBtn) {
+        this.#dropdownList.classList.remove('prefer__select-list-visible');
+        this.#dropdownBtn.classList.remove('prefer__select-btn--active');
+      }
+    });
+
+    //Press on Esc or Tab closed dropdown.
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' || e.key === 'Tab') {
+        this.#dropdownList.classList.remove('prefer__select-list-visible');
+      }
+    });
   }
 
   createMarkup() {
@@ -20,9 +62,5 @@ export default class Select {
     const allMarkup = this.createMarkup();
 
     this.#el.innerHTML = allMarkup;
-  }
-
-  onSelectChange(albumId) {
-    return albumId;
   }
 }
