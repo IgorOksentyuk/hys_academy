4a401e787a722cc760d3c86483e355dd74c7c7ba
Igor Oksentyuk <winbka.lem@gmail.com>
2023-09-11 18:37:26 +0300

    Removed index

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'HYS Academy',
       filename: 'index.html',
-      template: 'index.html',
+      template: 'src/html__css/index.html',
     }),
     new SpriteLoaderPlugin(),
     new webpack.ProvidePlugin({
