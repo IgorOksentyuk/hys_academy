e33200be4448540a024e118f85f06935d8cb47c7
Igor Oksentyuk <winbka.lem@gmail.com>
2023-09-12 11:47:24 +0300

    Changed scss path

--- src/html__css/ts/index.ts
+++ src/html__css/ts/index.ts
@@ -1,19 +1,19 @@
-import '../styles/style.scss';
+import "../../../styles.scss";
 
-import '../images/sprite.svg';
-import '../images/courses-block/woman-design.png';
-import '../images/courses-block/woman-marketing.png';
-import '../images/courses-block/woman-web.png';
-import '../images/tour-block/rectangle.png';
-import '../images/customers-block/customers-photo.png';
+import "../images/sprite.svg";
+import "../images/courses-block/woman-design.png";
+import "../images/courses-block/woman-marketing.png";
+import "../images/courses-block/woman-web.png";
+import "../images/tour-block/rectangle.png";
+import "../images/customers-block/customers-photo.png";
 
-import 'jquery';
-import './mobile-menu.ts';
-import './paginator.ts';
-import './slick.min.ts';
-import './slider.ts';
+import "jquery";
+import "./mobile-menu.ts";
+import "./paginator.ts";
+import "./slick.min.ts";
+import "./slider.ts";
 
-import App from './app';
+import App from "./app";
 
 const app = new App();
 app.init();
